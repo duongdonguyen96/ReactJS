df175c0b8bc48953912ffbbe994237af0a745d49
nguyendd1 <nguyendd1@scb.com.vn>
2021-06-07 15:35:13 +0700

    Pagination_Api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import PostList from './PostList';
+import Pagination from './Pagination';
+import queryString from 'query-string';
+import React, { Component }  from 'react';
+
 
 function App() {
+  const [postList, setPostList] = useState([])
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 70,
+  });
+
+  const [filters, setFilters] = useState({
+    _limit: 5,
+    _page: 1,
+  })
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const paramString = queryString.stringify(filters)
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramString}`;
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        console.log({ responseJSON })
+
+        const { data,pagination } = responseJSON;
+        setPostList(data)
+        setPagination(pagination)
+      } catch (error) {
+        console.log('Failed', error.message)
+
+      }
+    }
+    fetchPostList();
+  }, [filters]);
+
+
+  function handlePageChange(newPage) {
+    console.log('new page:', newPage)
+    setFilters({
+      ...filters,
+      _page:newPage,
+
+    })
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetch API</h1>
+      <PostList posts={postList} />
+      <Pagination
+        pagination={pagination}
+        onPageChange={handlePageChange}/>
     </div>
   );
 }
